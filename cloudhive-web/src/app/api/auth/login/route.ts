2b83b148fb4569998f2e5a442ea683343be5e3ae
Ayush Katiyar <148551722+aerick8602@users.noreply.github.com>
2025-04-26 12:27:41 +0530

    modigy api

--- cloudhive-web/src/app/api/auth/login/route.ts
+++ cloudhive-web/src/app/api/auth/login/route.ts
@@ -1,32 +1,24 @@
-// pages/api/auth/set.ts
-
 import { adminAuth } from "@/firebase/config/firebase-admin";
 
 const COOKIE_EXPIRES_IN = 60 * 60 * 24 * 5 * 1000; // 5 days in milliseconds
 
-export async function POST(req: any) {
-  const { idToken } = await req.json();
-  if (!idToken) {
-    return new Response("ID token is required", { status: 400 });
-  }
-
+export async function POST(request: Request) {
   try {
-    // Verify the ID token using Firebase Admin SDK
+    const { idToken } = await request.json();
+
+    if (!idToken) {
+      return new Response("ID token is required", { status: 400 });
+    }
+
+    // Verify the ID token
     const decodedToken = await adminAuth.verifyIdToken(idToken);
 
-    // Create the session cookie
+    // Create session cookie
     const sessionCookie = await adminAuth.createSessionCookie(idToken, {
       expiresIn: COOKIE_EXPIRES_IN,
     });
 
-    // Set the session cookie in the response header
-    const cookieOptions = {
-      httpOnly: true,
-      secure: process.env.ENV === "production", // Set secure flag in production
-      maxAge: COOKIE_EXPIRES_IN,
-      path: "/", // Available for the entire app
-    };
-
+    // Set session cookie
     return new Response("Session cookie set successfully", {
       status: 200,
       headers: {
