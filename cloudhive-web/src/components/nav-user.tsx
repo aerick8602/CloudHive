047501c9e56a2430591739af1f7afa5edcb0c96d
Ayush Katiyar <148551722+aerick8602@users.noreply.github.com>
2025-04-29 23:16:50 +0530

    nav user

--- cloudhive-web/src/components/nav-user.tsx
+++ cloudhive-web/src/components/nav-user.tsx
@@ -76,7 +76,7 @@ export function NavUser() {
             <DropdownMenuTrigger asChild>
               <SidebarMenuButton
                 size="lg"
-                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground w-[97%]"
               >
                 <Avatar className="h-8 w-8 rounded-lg">
                   <AvatarImage
