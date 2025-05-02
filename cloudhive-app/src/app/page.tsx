5b391a1c81912f79d209cc2fe7a093f04ac6c787
Ayush Katiyar <148551722+aerick8602@users.noreply.github.com>
2025-05-02 22:14:09 +0530

    new app

---
+++ cloudhive-app/src/app/page.tsx
@@ -0,0 +1,73 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import { ModeToggle } from "@/components/ui/mode-toggle";
+import { SearchForm } from "@/components/search-form";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+
+export default function Page() {
+  return (
+    <SidebarProvider>
+      <AppSidebar variant="inset" />
+      <SidebarInset className="overflow-hidden">
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 justify-between">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
+            />
+            <SearchForm className="w-full sm:ml-auto sm:w-auto" />
+          </div>
+          <div className="px-5">
+            <ModeToggle />
+          </div>
+        </header>
+
+        <main className=" h-[calc(100vh-5rem)] overflow-y-auto  flex flex-col gap-4 p-4 pt-0">
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+
+          {/* Your scrollable content here */}
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
